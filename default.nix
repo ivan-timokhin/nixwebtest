4b147fa079f01257bdd1b12ca23f4c6aaca74723
Ivan Timokhin <dev@ivan.timokhin.name>
2021-09-14 18:08:56 +0300

    Open and maximize a Chromium window on a client with selenium
    insertPythonPaths is obviously a horrible (and somewhat fragile, given
    Python versions) hack, but I have found no other obvious way to add
    Python packages to the testScript environment.  Also, there does not
    appear to be any direct access from the runner to the VMs, so I opted
    to forward the port instead; from my rather unscientific experiments
    it does not seem to leek outside of a builder, but IDK.

--- default.nix
+++ default.nix
@@ -5,6 +5,21 @@ let
   };
 
   user = "alice";
+
+  seleniumPort = 4444;
+
+  pythonPkgs = pkgs.python3Packages;
+
+  selenium = pythonPkgs.selenium;
+
+  extraPythonPackages = pythonPkgs.requiredPythonModules [ selenium ];
+
+  insertPythonPaths = pkgs.lib.strings.concatMapStringsSep "\n" (
+    drv:
+    if drv ? pythonModule
+    then ''sys.path.insert(0, "${drv}/lib/${drv.pythonModule.libPrefix}/site-packages")''
+    else ""
+  ) extraPythonPackages;
 in
 pkgs.nixosTest ({
   nodes = {
@@ -20,11 +35,16 @@ pkgs.nixosTest ({
       environment = {
         systemPackages = [
           pkgs.chromium
+          pkgs.selenium-server-standalone
         ];
 
         variables = {
           "XAUTHORITY" = "/home/${user}/.Xauthority";
         };
+      };
+
+      networking.firewall = {
+        allowedTCPPorts = [ seleniumPort ];
       };
     };
   };
@@ -32,16 +52,27 @@ pkgs.nixosTest ({
   testScript = ''
     import shlex
 
+    import sys
+
+    ${insertPythonPaths}
+
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+
     def ru(cmd):
         return "su - ${user} -c " + shlex.quote(cmd)
 
     start_all()
-    print("Start")
     client.wait_for_x()
     client.screenshot("initial")
-    client.succeed(ru("ulimit -c unlimited; chromium & disown"))
-    client.wait_for_window("Chromium")
-    client.sleep(10)
-    client.screenshot("chromium")
+    client.succeed(ru("ulimit -c unlimited; selenium-server & disown"))
+    client.wait_for_open_port(${toString seleniumPort})
+    client.screenshot("after")
+
+    options = Options()
+    client.forward_port(${toString seleniumPort}, ${toString seleniumPort})
+    with webdriver.Remote(options=options) as driver:
+        driver.maximize_window()
+        client.screenshot("maximized")
   '';
 })
