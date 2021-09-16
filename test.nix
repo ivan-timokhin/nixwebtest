745b8af8dc7e363cdf7211007b3e2ccc8a3bd20c
Ivan Timokhin <dev@ivan.timokhin.name>
2021-09-16 15:34:50 +0300

    Use niv to manage nixpkgs versions
    Seems easier and less maintenance-intensive than doing the same
    manually.
    
    The two nixpkgs branches were chosen because
    
    1. nixos channel-tracking branches are only updated when NixOS tests
    pass, which include VM tests of the sort I’m using here (including
    with selenium), reducing the probability something breaks beneath me.
    
    2. I would ideally like to support both people sticking to the latest
    release and those living on the edge.

--- test.nix
+++ test.nix
@@ -1,41 +1,52 @@
+{ sources ? import ./nix/sources.nix }:
 let
-  nixpkgs = fetchTarball
-    "https://github.com/NixOS/nixpkgs/archive/e85f0175e3effe9ba191d66c09e8f1b7d6362d5e.tar.gz";
-  pkgs = import nixpkgs { };
-  runner = import ./. { inherit pkgs; };
+  nixos = import sources.nixos { };
+  nixos-unstable = import sources.nixos-unstable { };
   name = "tester";
-in pkgs.linkFarm "webtest-nix-tests" [
-  {
-    name = "${name}-inline-script";
-    path = runner {
-      name = "${name}-inline-script";
-      browsers = b: [ b.firefox b.chromium ];
+  tests = { pkgs }:
+    let runner = import ./. { inherit pkgs; };
+    in pkgs.linkFarm "webtest-nix-tests" [
+      {
+        name = "${name}-inline-script";
+        path = runner {
+          name = "${name}-inline-script";
+          browsers = b: [ b.firefox b.chromium ];
 
-      script = ''
-        print("Hello from an inline script")
-        driver.fullscreen_window()
-        client.screenshot("iscript")
-      '';
-    };
+          script = ''
+            print("Hello from an inline script")
+            driver.fullscreen_window()
+            client.screenshot("iscript")
+          '';
+        };
+      }
+      {
+        name = "${name}-file-script";
+        path = runner {
+          name = "${name}-file-script";
+          browsers = b: [ b.firefox b.chromium ];
+
+          script = ./script.py;
+        };
+      }
+      {
+        name = "${name}-fun-script";
+        path = runner {
+          name = "${name}-fun-script";
+          browsers = b: [ b.firefox b.chromium ];
+
+          script = { nodes, ... }: ''
+            client.screenshot("${nodes.client.config.test-support.displayManager.auto.user}")
+          '';
+        };
+      }
+    ];
+in nixos.linkFarm "webtest-nix-multi-tests" [
+  {
+    name = "nixos";
+    path = tests { pkgs = nixos; };
   }
   {
-    name = "${name}-file-script";
-    path = runner {
-      name = "${name}-file-script";
-      browsers = b: [ b.firefox b.chromium ];
-
-      script = ./script.py;
-    };
-  }
-  {
-    name = "${name}-fun-script";
-    path = runner {
-      name = "${name}-fun-script";
-      browsers = b: [ b.firefox b.chromium ];
-
-      script = { nodes, ... }: ''
-        client.screenshot("${nodes.client.config.test-support.displayManager.auto.user}")
-      '';
-    };
+    name = "nixos-unstable";
+    path = tests { pkgs = nixos-unstable; };
   }
 ]
