cfc4eca1906dae48a67efc395c172842c5c67dbd
Ivan Timokhin <dev@ivan.timokhin.name>
2021-09-26 15:01:09 +0300

    Rearrange tests so that stable/unstable is each a full test suite
    …and the top-level derivation is just a linkfarm of them.  The
    advantage is the ability to run full test suite on just one of the
    test channels, plus assurance that output is identical on both
    channels; the disadvantage is inability to run tests on both without
    checking output in a single command.

--- shell.nix
+++ shell.nix
@@ -8,7 +8,7 @@ in pkgs.mkShell {
       exec ${pkgs.findutils}/bin/find . -path ./nix -prune -o -name '*.nix' -execdir ${pkgs.nixfmt}/bin/nixfmt {} +
     '')
     (pkgs.writeShellScriptBin "update-test-listing" ''
-      ${pkgs.findutils}/bin/find -L $(nix-build tests --no-out-link -A run-all-tests) -printf '%y %P\n'\
+      ${pkgs.findutils}/bin/find -L $(nix-build tests --no-out-link -A stable.run-all-tests) -printf '%y %P\n'\
        | LC_ALL=C ${pkgs.coreutils}/bin/sort -k2 > tests/test-output
     '')
   ];
--- tests/default.nix
+++ tests/default.nix
@@ -61,124 +61,133 @@ let
   };
 
   testOn = name: pkgs:
-    let runner = import ../. { inherit pkgs; };
-    in {
-      script-types = runner.testMany {
-        name = "${name}-scripts";
-        browsers = b: [ b.chromium ];
+    let
+      runner = import ../. { inherit pkgs; };
+      tests = {
+        script-types = runner.testMany {
+          name = "${name}-scripts";
+          browsers = b: [ b.chromium ];
 
-        nodes = { inherit webserver; };
+          nodes = { inherit webserver; };
 
-        scripts = {
-          inline = "open('done', 'w')";
+          scripts = {
+            inline = "open('done', 'w')";
 
-          path = ./css-check.py;
+            path = ./css-check.py;
 
-          function-inline = { nodes, ... }:
-            assert nodes.webserver.config.test-word == test-word;
+            function-inline = { nodes, ... }:
+              assert nodes.webserver.config.test-word == test-word;
+              "open('done', 'w')";
+
+            function-path = { nodes, ... }:
+              assert nodes.webserver.config.test-word == test-word;
+              ./css-check.py;
+          };
+        };
+
+        browsers = runner.test {
+          name = "${name}-browsers";
+
+          browsers = builtins.attrValues;
+
+          nodes = { inherit webserver; };
+
+          script = ''
+            webserver.start()
+            webserver.wait_for_open_port(80)
+
+            driver.get("http://webserver")
+            p = driver.find_element_by_css_selector("body > p")
+            assert p.text == "${test-word}"
+            open("done", "w")
+          '';
+        };
+
+        screenshots = runner.testMany {
+          name = "${name}-screenshots";
+
+          browsers = b: [ b.firefox ];
+
+          nodes = { };
+
+          scripts = {
+            selenium = "driver.save_screenshot('screen.png')";
+            nixos = "client.screenshot('screen')";
+          };
+        };
+
+        client-config = runner.test {
+          name = "${name}-cc";
+
+          browsers = b: [ b.firefox ];
+
+          nodes = { };
+
+          extraClientConfig = test-module;
+
+          script = { nodes, ... }:
+            assert nodes.client.config.test-word == test-word;
             "open('done', 'w')";
-
-          function-path = { nodes, ... }:
-            assert nodes.webserver.config.test-word == test-word;
-            ./css-check.py;
         };
-      };
 
-      browsers = runner.test {
-        name = "${name}-browsers";
+        client-memory = runner.test {
+          name = "${name}-cm";
 
-        browsers = builtins.attrValues;
+          browsers = b: [ b.firefox ];
 
-        nodes = { inherit webserver; };
+          nodes = { };
 
-        script = ''
-          webserver.start()
-          webserver.wait_for_open_port(80)
+          extraClientConfig = { virtualisation.memorySize = 768; };
 
-          driver.get("http://webserver")
-          p = driver.find_element_by_css_selector("body > p")
-          assert p.text == "${test-word}"
-          open("done", "w")
-        '';
-      };
-
-      screenshots = runner.testMany {
-        name = "${name}-screenshots";
-
-        browsers = b: [ b.firefox ];
-
-        nodes = { };
-
-        scripts = {
-          selenium = "driver.save_screenshot('screen.png')";
-          nixos = "client.screenshot('screen')";
+          script = { nodes, ... }:
+            assert nodes.client.config.virtualisation.memorySize == 768;
+            "open('done', 'w')";
         };
+
+        single = runner.testSingleBrowser {
+          name = "${name}-single";
+
+          browser = runner.browsers.firefox-esr;
+
+          nodes = { };
+
+          script = ''
+            open(driver.capabilities['browserName'], 'w')
+          '';
+        };
+
+        defaults = runner.test {
+          name = "${name}-defaults";
+
+          nodes = { };
+
+          script = ''
+            open(driver.capabilities['browserName'], 'w')
+          '';
+        };
+
+        examples = import ../examples.nix { inherit pkgs; };
       };
-
-      client-config = runner.test {
-        name = "${name}-cc";
-
-        browsers = b: [ b.firefox ];
-
-        nodes = { };
-
-        extraClientConfig = test-module;
-
-        script = { nodes, ... }:
-          assert nodes.client.config.test-word == test-word;
-          "open('done', 'w')";
-      };
-
-      client-memory = runner.test {
-        name = "${name}-cm";
-
-        browsers = b: [ b.firefox ];
-
-        nodes = { };
-
-        extraClientConfig = { virtualisation.memorySize = 768; };
-
-        script = { nodes, ... }:
-          assert nodes.client.config.virtualisation.memorySize == 768;
-          "open('done', 'w')";
-      };
-
-      single = runner.testSingleBrowser {
-        name = "${name}-single";
-
-        browser = runner.browsers.firefox-esr;
-
-        nodes = { };
-
-        script = ''
-          open(driver.capabilities['browserName'], 'w')
-        '';
-      };
-
-      defaults = runner.test {
-        name = "${name}-defaults";
-
-        nodes = { };
-
-        script = ''
-          open(driver.capabilities['browserName'], 'w')
-        '';
-      };
-
-      examples = import ../examples.nix { inherit pkgs; };
-    };
+      run-all-tests = import ./linkfarm.nix pkgs "nwt-tests" tests;
+    in (pkgs.runCommand "nwt-tests-check-out" { } ''
+      mkdir $out
+      find -L ${run-all-tests} -printf '%y %P\n' | sort -k2 > $out/list
+      diff $out/list ${./test-output}
+    '').overrideAttrs
+    (oldAttrs: { passthru = { inherit tests run-all-tests; }; });
 
   tests = builtins.mapAttrs testOn test-pkgs;
 
-  run-all-tests = import ./linkfarm.nix pkgs "nixwebtest-selftests" tests;
+in (import ./linkfarm.nix pkgs "nwt-tests-all" tests).overrideAttrs
+(oldAttrs: { passthru = tests; })
 
-in (pkgs.runCommand "nixwebtest-check-output" { } ''
-  mkdir $out
-  find -L ${run-all-tests} -printf '%y %P\n' | sort -k2 > $out/list
-  diff $out/list ${./test-output}
-'').overrideAttrs (oldAttrs: {
-  passthru = {
-    inherit tests;
-    inherit run-all-tests;
-  };
-})
+# (pkgs.runCommand "nixwebtest-check-output" { } ''
+#   mkdir $out
+#   find -L ${run-all-tests} -printf '%y %P\n' | sort -k2 > $out/list
+#   diff $out/list ${./test-output}
+# '').overrideAttrs (oldAttrs: {
+#   passthru = {
+#     inherit tests;
+#     inherit run-all-tests;
+#   };
+# })
